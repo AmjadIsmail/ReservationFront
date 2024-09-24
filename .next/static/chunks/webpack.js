103aa4f3401d9db91992b218465c3018a38849d6
matif250 <atifmuhammad24@gmail.com>
2024-09-24 12:58:31 +0500

    fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ee499368a202de33"; }
+/******/ 		__webpack_require__.h = function() { return "b37935bc489a2b16"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
